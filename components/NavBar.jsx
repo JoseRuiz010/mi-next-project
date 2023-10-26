b37783bb9a64143d95c0212cb55fb14ecd0609dc
joseruiz010 <joruiz765@gmail.com>
2023-10-25 22:42:23 -0300

    etiqueta metadata y layout

---
+++ components/NavBar.jsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>Navbar</h1>
+      <ul>
+        <li>
+          <Link href="/" title="Home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/tienda">Tiendas</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
