3197f7481a01486e8bc2a873595226363baed5fe
joseruiz010 <joruiz765@gmail.com>
2023-10-25 23:02:01 -0300

    componentes clientes y del servidor

--- app/tienda/productos/page.jsx
+++ app/tienda/productos/page.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const productos = () => {
+  const [count, setcount] = useState(0)
   return (
-    <div>productos</div>
+    <section>
+      productos {count}
+
+      <button onClick={() => setcount(prev => prev + 1)}>+ 1</button>
+    </section>
   )
 }
 
