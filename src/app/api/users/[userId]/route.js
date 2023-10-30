ae143842330b59d5a25bb4288f761d1a21d6e1ee
joseruiz010 <joruiz765@gmail.com>
2023-10-29 23:42:51 -0300

    variables de entorno

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -3,9 +3,8 @@ import { NextResponse } from "next/server"
 export const GET = async (request, { params }) => {
   const urlParams = new URL(request.url)
   const { searchParams } = urlParams
-  console.log('====================================');
-  console.log(urlParams, searchParams);
-  console.log('====================================');
+  console.log(process.env.TOKEN)
+  console.log(process.env.SECRET_KEY)
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`);
   const user = await res.json();
   return NextResponse.json({
